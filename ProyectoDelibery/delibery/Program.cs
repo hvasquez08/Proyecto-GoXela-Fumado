5c6aeb085233f60a818e20324d1bea01133e9e51
williamalvarez755 <williamalvarez1101@gmail.com>
2026-08-25 19:55:17 -0600

    primer commint con la primera clase y probando si lo unimos correctamente

---
+++ ProyectoDelibery/delibery/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delibery
+{
+    internal class Program
+    {
+        public class Persona{
+            private string codigo ;
+
+            public  Codigo
+            {
+                get { return codigo; }
+                set { codigo = value; }
+            }
+
+            private string nombrecompleto;
+
+            public string NombreCompleto
+            {
+                get { return nombrecompleto; }
+                set { nombrecompleto = value; }
+            }
+            
+            private string telefono
+            {
+                get { return telefono; }
+                set { telefono = value; }
+            }
+
+
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
